6cb34d11d02f3ad0152cd39e25c3dd486cd416fe
Csyeoh <csyeoh0726@gmail.com>
2025-03-22 14:01:27 +0800

    Base

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="max-w-full">
+      <h1 className="text-red-500 text-4xl font-bold text-center">Hello world</h1>
+    </div>
+  )
+}
+
+export default App
