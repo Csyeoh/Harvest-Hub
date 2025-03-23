57b167315ebec9c34480fdbf9e2837d62d261da6
Csyeoh <csyeoh0726@gmail.com>
2025-03-23 16:26:40 +0800

    dashboard

---
+++ src/components/SoilStatus.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { FaTint, FaAtom } from 'react-icons/fa';
+
+const SoilStatus = () => {
+  return (
+    <div className="soil-status">
+      <div className="soil-card">
+        <FaTint className="soil-icon" />
+        <p>Soil moisture state: <span className="status-excellent">excellent</span></p>
+      </div>
+      <div className="soil-card">
+        <FaAtom className="soil-icon" />
+        <p>Soil pH state: <span className="status-excellent">excellent</span></p>
+      </div>
+    </div>
+  );
+};
+
+export default SoilStatus;
